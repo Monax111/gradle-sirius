9bc93bff37fde050094b87b299e374af215e672b
tim <t.p.maerov@tinkoff.ru>
2021-06-11 14:18:54 +0300

    gradle begin

--- begin/versionConflict/projectD/build.gradle.kts
+++ begin/versionConflict/projectD/build.gradle.kts
@@ -12,7 +12,4 @@ dependencies {
     implementation(group = "ru.demo.conflict", name = "projectB", version = "1")
     implementation(group = "ru.demo.conflict", name = "projectC", version = "2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-
 }
