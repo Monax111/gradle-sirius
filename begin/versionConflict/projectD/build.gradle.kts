c66e4d63ec8ef7cca957e0eec92260b2c4ac8021
tim <t.p.maerov@tinkoff.ru>
2021-06-11 14:23:09 +0300

    gradle begin

--- begin/versionConflict/projectD/build.gradle.kts
+++ begin/versionConflict/projectD/build.gradle.kts
@@ -12,4 +12,11 @@ dependencies {
     implementation(group = "ru.demo.conflict", name = "projectB", version = "1")
     implementation(group = "ru.demo.conflict", name = "projectC", version = "2")
 
+    constraints {
+        implementation("ru.demo.conflict:projectA"){
+            version {
+                strictly("2")
+            }
+        }
+    }
 }
